f5e80ad2afb41b422ce9c4d46bb1c0e01858dab4
Naomi <99690752+Naomi-sann@users.noreply.github.com>
2023-06-01 07:35:31 +0000

    Update AnjamMidam.jsx

--- src/pages/AnjamMidam/AnjamMidam.jsx
+++ src/pages/AnjamMidam/AnjamMidam.jsx
@@ -4,7 +4,7 @@ const AnjamMidam = () => {
   return (
     <FullPageHeight centerElements>
       <h1 style={{ fontSize: "45px" }}>
-        <a href="https://anjammidam.com/mohammadsalehamani">انجام میدم</a>
+        <a href="https://anjammidam.com/mohammadsalehamani" target="_blank" rel="noreferrer">انجام میدم</a>
       </h1>
     </FullPageHeight>
   );
